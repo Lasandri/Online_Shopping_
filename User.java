7333e2ed798a2e35f433c436df7b9343da6da96c
Lasandri silva <120217797+Lasandri@users.noreply.github.com>
2023-12-11 15:56:13 +0530

    initial commit

--- User.java
+++ User.java
@@ -17,4 +17,9 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User [username= "+userName+", Password= "+password+" ]";
+    }
 }
