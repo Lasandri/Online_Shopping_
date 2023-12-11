d6c9bcfd377f1fe929eef73dc23a08a3c5964f8f
Lasandri silva <120217797+Lasandri@users.noreply.github.com>
2023-12-11 16:11:33 +0530

    initial commit

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
     private List<Product> products;
+
+    public ShoppingCart(List<Product> products) {
+        this.products = products;
+    }
+
+    public ShoppingCart(){
+    this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public void removeProduct(Product product){
+        products.remove(product);
+    }
+
 }
